bbdbc0fdee7fba813b85b3310c9d86441d533a9a
VVVofka <vvvofka@yandex.ru>
2022-08-14 09:33:03 +0300

    dlg?

--- MultiAmp/CMCell.cpp
+++ MultiAmp/CMCell.cpp
@@ -1,2 +1,12 @@
 #include "pch.h"
 #include "CMCell.h"
+BEGIN_MESSAGE_MAP(CMCell, CStatic)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+void CMCell::OnPaint(){
+	CPaintDC dc(this); // device context for painting
+					   // TODO: Add your message handler code here
+					   // Do not call CStatic::OnPaint() for painting messages
+}
--- MultiAmp/CMCell.h
+++ MultiAmp/CMCell.h
@@ -3,5 +3,7 @@ class CMCell :public CStatic {
 	DECLARE_DYNAMIC(CMCell)
 public:
 
+	DECLARE_MESSAGE_MAP()
+	afx_msg void OnPaint();
 };
 
