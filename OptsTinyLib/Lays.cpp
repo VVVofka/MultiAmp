f4363bea1b6fbf42095a557c86bde80401071631
VVVofka <vvvofka@yandex.ru>
2022-08-27 23:14:54 +0300

    to slaap

--- OptsTinyLib/Lays.cpp
+++ OptsTinyLib/Lays.cpp
@@ -3,12 +3,14 @@
 #include <string>
 
 Lays::Lays(){
-	cfg.topX = cfg.topY = 1;
-	cfg.laysCnt = 5;
+	cfg.vkf.resize(cfg.laysCnt, 1.0f);
 } // ///////////////////////////////////////////////////////////////////////////////////
 XMLNode* Lays::create(XMLNode* parent_node){
-	structLaysCfg cfg;
-	return set(parent_node, cfg);;
+	structLaysCfg new_cfg;
+	new_cfg.topX = new_cfg.topY = 1;
+	new_cfg.laysCnt = 5;
+	new_cfg.vkf.resize(new_cfg.laysCnt, 1.0f);
+	return set(parent_node, new_cfg);
 } // ///////////////////////////////////////////////////////////////////////////////////
 XMLNode* Lays::load(XMLNode* parent_node){
 	node = NULL;
@@ -20,6 +22,11 @@ XMLNode* Lays::load(XMLNode* parent_node){
 			cfg.topX = ele->IntAttribute("topX", 1);
 			cfg.topY = ele->IntAttribute("topY", 1);
 			cfg.laysCnt = ele->IntAttribute("cnt", 5);
+			cfg.vkf.resize(cfg.laysCnt);
+			for(size_t j = 0; j < cfg.vkf.size(); j++){
+				std::string atrname = "klayf" + std::to_string(j);
+				cfg.vkf[j] = ele->FloatAttribute(atrname.c_str(), 1.0f);
+			}
 			parent_node->DeleteChild(curnode);
 			node = set(parent_node, cfg);
 			break;
@@ -27,9 +34,9 @@ XMLNode* Lays::load(XMLNode* parent_node){
 	}
 	return node;
 } // ///////////////////////////////////////////////////////////////////////////////////
-XMLNode* Lays::set(XMLNode* parent_node, const structLaysCfg& cfg){
+XMLNode* Lays::set(XMLNode* parent_node, const structLaysCfg& new_cfg){
 	isChange = false;
-
+	cfg = new_cfg;
 	// If exist, delete node *****************
 	for(XMLNode* curnode = parent_node->FirstChild(); curnode; curnode = curnode->NextSibling()){
 		XMLElement* ele = curnode->ToElement();
@@ -43,10 +50,13 @@ XMLNode* Lays::set(XMLNode* parent_node, const structLaysCfg& cfg){
 	XMLDocument* doc = parent_node->GetDocument();
 	XMLElement* ele_out = doc->NewElement(XMLName);
 
-	ele_out->SetAttribute("topX", cfg.topX);	// 1
-	ele_out->SetAttribute("topY", cfg.topY);	// 1
-	ele_out->SetAttribute("cnt", cfg.laysCnt);	// 5
-
+	ele_out->SetAttribute("topX", new_cfg.topX);	// 1
+	ele_out->SetAttribute("topY", new_cfg.topY);	// 1
+	ele_out->SetAttribute("cnt", new_cfg.laysCnt);	// 5
+	for(size_t j = 0; j < new_cfg.vkf.size(); j++){
+		std::string atrname = "klayf" + std::to_string(j);
+		ele_out->SetAttribute(atrname.c_str(), new_cfg.vkf[j]);	// 1.0f
+	}
 	node = parent_node->InsertEndChild(ele_out);
 	return node;
 } // ///////////////////////////////////////////////////////////////////////////////////
--- OptsTinyLib/structLaysCfg.h
+++ OptsTinyLib/structLaysCfg.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <vector>
 struct structLaysCfg{
 	size_t topX = 1, topY = 1;
 	size_t laysCnt = 5;
+	std::vector<float> vkf;
 };
