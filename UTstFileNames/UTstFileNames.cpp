5c2d03bfc6d4e7cd422edac46e62a8c4f92d94bb
VVVofka <vvvofka@yandex.ru>
2022-10-06 12:44:54 +0300

    before ar2

--- UTstFileNames/UTstFileNames.cpp
+++ UTstFileNames/UTstFileNames.cpp
@@ -18,7 +18,24 @@ public:
 	TEST_METHOD(TestMethod1){
 		auto s = curDir();
 		Assert::IsTrue(s.size() > 0);
+#ifdef _DEBUG
+
+#ifdef MYWIN32
 		Assert::AreEqual(0, strcmp(s.c_str(), "C:\\Prog\\CPP\\MultiAmp\\Win32\\Debug"));
+#else // WIN32
+		Assert::AreEqual(0, strcmp(s.c_str(), "C:\\Prog\\CPP\\MultiAmp\\x64\\Debug"));
+#endif // WIN32
+
+#else // DEBUG
+
+#ifdef MYWIN32
+		Assert::AreEqual(0, strcmp(s.c_str(), "C:\\Prog\\CPP\\MultiAmp\\Win32\\Release"));
+#else // WIN32
+		Assert::AreEqual(0, strcmp(s.c_str(), "C:\\Prog\\CPP\\MultiAmp\\x64\\Release"));
+#endif // WIN32
+
+#endif // DEBUG
+
 	}
 	};
 }
