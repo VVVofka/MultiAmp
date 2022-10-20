a6200b554a75caf626186c44f14ee06657a61912
VVVofka <vvvofka@yandex.ru>
2022-10-20 14:01:55 +0300

    CreateComputeShader+

--- WinDxDLL/MDX.h
+++ WinDxDLL/MDX.h
@@ -275,22 +275,7 @@ protected:
 		// Present the information rendered to the back buffer to the front buffer (the screen)
 		g_pSwapChain->Present(0, 0);
 	} // ///////////////////////////////////////////////////////////////////////////////////////////////////
-	HRESULT CreateComputeShader(){
-		// Bind a resource view to the CS buffer
-		D3D11_BUFFER_DESC descBuf;
-		ZeroMemory(&descBuf, sizeof(descBuf));
-		g_pVertexPosBuffer->GetDesc(&descBuf);
-
-		D3D11_SHADER_RESOURCE_VIEW_DESC DescRV;
-		ZeroMemory(&DescRV, sizeof(DescRV));
-		DescRV.Format = DXGI_FORMAT_R32_TYPELESS;
-		DescRV.ViewDimension = D3D11_SRV_DIMENSION_BUFFEREX;
-		DescRV.BufferEx.Flags = D3D11_BUFFEREX_SRV_FLAG_RAW;
-		DescRV.Buffer.FirstElement = 0;
-		DescRV.Buffer.NumElements = descBuf.ByteWidth / sizeof(int);
-		RETURN_IF_FAIL(g_pd3dDevice->CreateShaderResourceView(g_pVertexPosBuffer, &DescRV, &g_pVertexPosBufferRV));
-		return S_OK;
-	} // /////////////////////////////////////////////////////////////////////////////////////////////
+	HRESULT CreateComputeShader(); // /////////////////////////////////////////////////////////////////////////////////////////////
 
 }; // ******************************************************************************************
 
