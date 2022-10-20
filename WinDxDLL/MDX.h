9d52a528bf05991c885d994e847457344284c419
VVVofka <vvvofka@yandex.ru>
2022-10-20 09:42:40 +0300

    before create MDX.cpp

--- WinDxDLL/MDX.h
+++ WinDxDLL/MDX.h
@@ -14,7 +14,7 @@
 #ifndef SAFE_DELETE
 #define SAFE_DELETE(p)       { if(p) { delete (p);     (p)=NULL; } }
 #endif    
-//
+
 //#ifndef SAFE_DELETE_ARRAY
 //#define SAFE_DELETE_ARRAY(p) { if(p) { delete[] (p);   (p)=NULL; } }
 //#endif    
