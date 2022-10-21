4a79821383a615decc8eaf8d2878585682d39377
VVVofka <vvvofka@yandex.ru>
2022-10-21 13:21:22 +0300

    #include "..\OptsTinyLib\structsAll.h" +

--- OptsTinyLib/structDataCfg.h
+++ OptsTinyLib/structDataCfg.h
@@ -1,16 +1,15 @@
 #pragma once
 #include <vector>
 #include <string>
-using namespace std;
 class structDataCfg{
 public:
-	vector<size_t> v;
-	string sigma;
-	string seed;
+	std::vector<size_t> v;
+	std::string sigma;
+	std::string seed;
 
 	float fsigma();
-	size_t fill_v(const string& s);
-	size_t fill_v_bak(const string& s);
-	string get_s() const;
+	size_t fill_v(const std::string& s);
+	size_t fill_v_bak(const std::string& s);
+	std::string get_s() const;
 };
 
