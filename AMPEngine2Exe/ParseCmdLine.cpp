779c17f93e540fb43ff92cbb38e83f301bee8630
VVVofka <vvvofka@yandex.ru>
2022-11-14 11:31:30 +0300

    before test parse

--- AMPEngine2Exe/ParseCmdLine.cpp
+++ AMPEngine2Exe/ParseCmdLine.cpp
@@ -1,7 +1,69 @@
 #include "pch.h"
 #include "ParseCmdLine.h"
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <locale>
+#include <codecvt>
+using namespace std;
+INT64 ParseCmdLine::parse(LPWSTR s_in){
+	tick_cnt = 0;
+	fname.clear();
+	
+	string s = ws2s(s_in);
+	trim(s);
+	size_t len = s.size();
+	
+	char delim;
+	size_t pos;
+	if(s[0] == '"' || s[0] == '\'' || s[0] == '`'){
+		delim = s[0];
+		pos = 1;
+	}
+	else{
+		delim = ' ';
+		pos = 0;
+		fname = s[0];
+	}
 
-INT64 ParseCmdLine::parse(LPWSTR s){
+	while(pos < len){
+		if(s[pos] == delim)
+			break;
+		fname += s[pos++];
+	}
+	if(fname.size() == 0 ) 
+		fname = def_fname;
 
-    return INT64(-1);
+	while(pos < len){
+		const int ch = s[pos] - '0';
+		if(ch >= 0 && ch <= 9)
+			tick_cnt = tick_cnt * 10 + ch;
+		pos++;
+	}
+	return tick_cnt;
+} // ////////////////////////////////////////////////////////////////////////////////////////////////////
+//std::wstring ParseCmdLine::trim(wchar_t const* str){
+//} // ////////////////////////////////////////////////////////////////////////////////////////////////////
+std::string& ParseCmdLine::ltrim(std::string& s){
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+		std::ptr_fun<int, int>(std::isgraph)));
+	return s;
+} // ////////////////////////////////////////////////////////////////////////////////////////////////////
+std::string& ParseCmdLine::rtrim(std::string& s){
+	s.erase(std::find_if(s.rbegin(), s.rend(),
+		std::ptr_fun<int, int>(std::isgraph)).base(), s.end());
+	return s;
+} // ////////////////////////////////////////////////////////////////////////////////////////////////////
+std::string& ParseCmdLine::trim(std::string& s){
+	return ltrim(rtrim(s));
+} // ////////////////////////////////////////////////////////////////////////////////////////////////////
+std::wstring ParseCmdLine::s2ws(const std::string& str){
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+	return converterX.from_bytes(str);
+} // ////////////////////////////////////////////////////////////////////////////////////////////////////
+std::string ParseCmdLine::ws2s(const std::wstring& wstr){
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+	return converterX.to_bytes(wstr);
 } // ////////////////////////////////////////////////////////////////////////////////////////////////////
--- AMPEngine2Exe/ParseCmdLine.h
+++ AMPEngine2Exe/ParseCmdLine.h
@@ -2,8 +2,17 @@
 #include <string>
 class ParseCmdLine{
 public:
-	std::string f_name;
+	std::string fname;
+	const char* def_fname = "defaultcfg.xml";
 	INT64 tick_cnt;
 	INT64 parse(LPWSTR s);
-}; // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+	static std::string& ltrim(std::string& s);
+	static std::string& rtrim(std::string& s);
+	static std::string& trim(std::string& s);
+	static std::wstring s2ws(const std::string& str);
+	static std::string ws2s(const std::wstring& wstr);
+private:
+
+};
 
