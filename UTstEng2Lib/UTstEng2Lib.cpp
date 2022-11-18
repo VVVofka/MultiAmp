3e8e25b0339067b4384943b1a7b204fe05b2232a
VVVofka <vvvofka@yandex.ru>
2022-11-18 15:13:24 +0300

    to video

---
+++ UTstEng2Lib/UTstEng2Lib.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UTstEng2Lib
+{
+	TEST_CLASS(UTstEng2Lib)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
