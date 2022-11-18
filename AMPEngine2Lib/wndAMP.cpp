31dfd3635a963f87bb36853c4394cefe63d7fc2d
VVVofka <vvvofka@yandex.ru>
2022-11-18 10:16:47 +0300

    no err

--- AMPEngine2Lib/wndAMP.cpp
+++ AMPEngine2Lib/wndAMP.cpp
@@ -3,7 +3,7 @@
 #include "MDX/MDX2.h"
 #include "..\OptsTinyLib\structAll.h"
 
-//namespace eng2{
+namespace eng2{
 	// static file
 	bool pauseRender;
 	bool isQuit;
@@ -35,22 +35,22 @@
 			wcex.lpszClassName = szWindowClass;
 			wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 			ATOM retReg = RegisterClassEx(&wcex);
-			if(retReg == 0)
+			if(retReg == 0){
 				ErrorExit(TEXT("InitWindow RegisterClassEx"));
-			if(!retReg)
 				return E_FAIL;
+			}
 		}
 		// Create window
 		RECT rc = {0, 0, 900, 900};
 		AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-		g_hWnd = CreateWindow(szWindowClass, L"AMPC++ and Direct3D 11 InterOp Sample",
+		g_hWnd = CreateWindow(szWindowClass, L"fps: 0",
 			WS_OVERLAPPEDWINDOW,
 			CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
 			NULL);
-		if(g_hWnd == 0)
+		if(g_hWnd == 0){
 			ErrorExit(TEXT("InitWindow CreateWindow"));
-		if(!g_hWnd)
 			return E_FAIL;
+		}
 		ShowWindow(g_hWnd, nCmdShow);
 		return S_OK;
 	} // //////////////////////////////////////////////////////////////////////////////////////////////
@@ -85,10 +85,11 @@
 		mdx.CleanupDevice();
 		return (int)msg.wParam;
 	} // /////////////////////////////////////////////////////////////////////////////
-	void ErrorExit(LPCTSTR caption){	// Retrieve the system error message for the last-error code
+// Retrieve the system error message for the last-error code
+// caption: TEXT("...")
+	void ErrorExit(LPCTSTR caption){
 		LPVOID lpMsgBuf, lpDisplayBuf;
 		DWORD dw = GetLastError();
-
 		FormatMessage(
 			FORMAT_MESSAGE_ALLOCATE_BUFFER |
 			FORMAT_MESSAGE_FROM_SYSTEM |
@@ -112,48 +113,48 @@
 		LocalFree(lpDisplayBuf);
 		//ExitProcess(dw);
 	} // //////////////////////////////////////////////////////////////////////////////////////////
-//} // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
-	switch(message){
-	case WM_PAINT:
-		PAINTSTRUCT ps;
-		BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code that uses hdc here...
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_KEYDOWN:
-		switch(wParam){
-		case VK_ESCAPE:
+	LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
+		switch(message){
+		case WM_PAINT:
+			PAINTSTRUCT ps;
+			BeginPaint(hWnd, &ps);
+			// TODO: Add any drawing code that uses hdc here...
+			EndPaint(hWnd, &ps);
+			break;
+		case WM_KEYDOWN:
+			switch(wParam){
+			case VK_ESCAPE:
+				isQuit = true;
+				SendMessage(hWnd, WM_CLOSE, 0, 0);
+				break;
+			case 79:{ // key 'o'  // I=73
+				pauseRender = true;
+				//if(model2.options.showDlg()){
+				//	model2.Create();
+				//	mdx.CleanupDevice();
+				//	mdx.InitDevice(g_hWnd, mdx.cfg_all);
+				//}
+				pauseRender = false;
+				break;
+			}
+			case VK_PAUSE:
+				break;
+			default:
+				//				model2.setConsole();
+				printf("%d\n", (int)wParam);
+				break;
+			}
+		case WM_DESTROY:
 			isQuit = true;
-			SendMessage(hWnd, WM_CLOSE, 0, 0);
+			PostQuitMessage(0);	// close application
+			//SendMessage(hWnd, WM_QUIT, 0, 0);
 			break;
-		case 79:{ // key 'o'  // I=73
-			pauseRender = true;
-			//if(model2.options.showDlg()){
-			//	model2.Create();
-			//	mdx.CleanupDevice();
-			//	mdx.InitDevice(g_hWnd, mdx.cfg_all);
-			//}
-			pauseRender = false;
-			break;
-		}
-		case VK_PAUSE:
-			break;
+			//return 0;
 		default:
-			//				model2.setConsole();
-			printf("%d\n", (int)wParam);
-			break;
+			if(--cfgall->misc.cntForStop == 0)
+				DestroyWindow(hWnd);
+			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
-	case WM_DESTROY:
-		isQuit = true;
-		PostQuitMessage(0);	// close application
-		//SendMessage(hWnd, WM_QUIT, 0, 0);
-		break;
-		//return 0;
-	default:
-		if(--cfgall->misc.cntForStop == 0)
-			DestroyWindow(hWnd);
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-} // ////////////////////////////////////////////////////////////////////////////////////
+		return 0;
+	} // ////////////////////////////////////////////////////////////////////////////////////
+} // namespace eng2 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
