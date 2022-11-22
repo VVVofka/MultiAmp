6a3665f949aa4616154e1d0ddbbe2a313b606fb5
VVVofka <vvvofka@yandex.ru>
2022-11-22 21:53:29 +0300

    to sleep 2

--- AMPEngine2Lib/Engine/ProcessA.cpp
+++ AMPEngine2Lib/Engine/ProcessA.cpp
@@ -7,10 +7,10 @@ ProcessA::ProcessA(Lays* p_lays, MaskA* p_Mask_a){
 void ProcessA::RunAll(const int_2 shift){		// return sum vabs from last lay
 	gpuRun0(shift);
 
-	LayMid* dn, up = lays->vMidLays[0];
+	LayMid  *dn, *up = lays->vMidLays[0];
 	if(up->cpuType != CPUtype::GPU)
 		up->gpu2cpu();
-	
+
 	for(int j = 1; j < lays->cntMidLays; j++){
 		dn = up;
 		up = lays->vMidLays[j];
