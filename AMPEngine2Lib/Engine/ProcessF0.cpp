f505aba0a0b8434bf9df11e4f6998aa47071950b
VVVofka <vvvofka@yandex.ru>
2022-11-29 10:27:18 +0300

    1

--- AMPEngine2Lib/Engine/ProcessF0.cpp
+++ AMPEngine2Lib/Engine/ProcessF0.cpp
@@ -4,6 +4,7 @@
 #define Y 0
 using namespace concurrency::graphics;
 using namespace concurrency::direct3d;
+using namespace Concurrency::fast_math;
 
 void ProcessF::gpuRun0(const uint_2 shift, const uint iter){
 	const LayMid* up_lay = lays->vMidLays[1];
@@ -20,17 +21,16 @@ void ProcessF::gpuRun0(const uint_2 shift, const uint iter){
 	const float_2 rSizeDn(lay_0->sz);
 
 	const concurrency::array<float_2, 1>& f_masks = *fmasks->vgpu;
-	uint_2 iter2 = uint_2(iter, iter ^ 1);
-	auto zzz = 0xb0111;
+	const uint signFor0 = (iter & 1) + 1; // 1 or 2
 
 	parallel_for_each(up_vgpu_a.extent,
 		[&dn_vgpu_a, &up_vgpu_a, &dn_vgpu_f, &up_vgpu_f, &screen,
-		&f_masks, shift, klayf, rSizeDn, iter2
+		&f_masks, shift, klayf, rSizeDn, signFor0
 		](index<2> idUp)restrict(amp) {
-			const uint x0 = (idUp[X] * 2 + shift.x) % dn_vgpu_a.extent[X];
-			const uint x1 = (x0 + 1) % dn_vgpu_a.extent[X];
-			const uint y0 = (idUp[Y] * 2 + shift.y) % dn_vgpu_a.extent[Y];
-			const uint y1 = (y0 + 1) % dn_vgpu_a.extent[Y];
+			//const uint x0 = (idUp[X] * 2 + shift.x) % dn_vgpu_a.extent[X];
+			//const uint x1 = (x0 + 1) % dn_vgpu_a.extent[X];
+			//const uint y0 = (idUp[Y] * 2 + shift.y) % dn_vgpu_a.extent[Y];
+			//const uint y1 = (y0 + 1) % dn_vgpu_a.extent[Y];
 
 			//index: 0(2,4,6) digit: 0:point absent or f=0; 
 			//						 1:point present;
@@ -44,150 +44,168 @@ void ProcessF::gpuRun0(const uint_2 shift, const uint iter){
 			 0,20, 0, 0,25,40,25,25, 0,20, 0, 0,17, 0,17, 0,  22,20,22,22, 0, 0, 0, 0,22,22,22,22,34, 0,34, 0,
 			 0,20, 0, 0,25,40,25,25, 0,20, 0, 0,17, 0,17, 0,   0,20, 0, 0, 0, 0, 0, 0, 0,20, 0, 0,17, 0,17, 0,
 			 0,20, 0, 0,25,40,25,25, 0,20, 0, 0,17, 0,17, 0,  22,20,22,22, 0, 0, 0, 0,22,22,22,22,34, 0,34, 0,
-			
+
 			 0,20, 0, 0,25,40,25,25, 0,20, 0, 0,17, 0,17, 0,  30,20,30,30,45,60,45,45,30,20,30,30, 0,30,17,30,
 			 0,20, 0, 0,25,40,25,25, 0,20, 0, 0,17, 0,17, 0,  22,20,22,22, 0, 0, 0, 0,22,22,22,22,34, 0,34, 0,
 			27,20,27,27,25,40,25,25,27, 0,27,27,27,27,17,27,   0,20, 0, 0, 0, 0, 0, 0, 0,20, 0, 0,17, 0,17, 0,
 			27, 0,27,27,27,27,27,27,27,27,27,27,27,27,51,27,  39,20,39,39, 0, 0, 0, 0,39,39,39,39,51, 0,51, 0
 			};
 
-			int idmask = up_vgpu_a[idUp] * 16;
-			float_2 curf = dn_vgpu_f[index<2>(idUp[Y] * 2, idUp[X] * 2)];
+			const int idmask = up_vgpu_a[idUp] * 16;	// TODO: define?
+			float upf = dn_vgpu_f[index<2>(idUp[Y] * 2, idUp[X] * 2)].x;
+			const int x0 = idUp[X] * 4;	// TODO: define?
+			const int y0 = (idUp[Y] * 4 + shift.y) % dn_vgpu_a.extent[Y];
+			const int idmove =
+				(((signbitf(upf + f_masks[idmask + 5].x * klayf) + signFor0) / 2) << 7) |
+				(sign(dn_vgpu_a[index<2>(y0, (x0 + shift.x + 3) % dn_vgpu_a.extent[X])] + 1) << 6) |
 
-			//uint_2 dn0 = mid0 * 2;
-			//uint_2 ofs = iter2;
+				(((signbitf(upf + f_masks[idmask + 4].x * klayf) + signFor0) / 2) << 5) |
+				(sign(dn_vgpu_a[index<2>(y0, (x0 + shift.x + 2) % dn_vgpu_a.extent[X])] + 1) << 4) |
 
-			//index<2> src = index<2>(dn0.y, dn0.x);
-			//index<2> dst = index<2>(dn0.y + ofs.y, dn0.x + ofs.x);
-			//float_2 fsrc = curf + f_masks[idmask] * klayf;
-			//float_2 fdst = curf + f_masks[idmask + 2 * ofs.y + ofs.x] * klayf;
+				(((signbitf(upf + f_masks[idmask + 1].x * klayf) + signFor0) / 2) << 3) |
+				(sign(dn_vgpu_a[index<2>(y0, (x0 + shift.x + 1) % dn_vgpu_a.extent[X])] + 1) << 2) |
 
+				(((signbitf(upf + f_masks[idmask].x * klayf) + signFor0) / 2) << 1) |
+				(sign(dn_vgpu_a[index<2>(y0, (x0 * 4 + shift.x) % dn_vgpu_a.extent[X])] + 1));
+			if(vmaskmov[idmove] & 0xb110000){
+				const int src = vmaskmov[idmove] & 0xb11;
+				const int dst = (vmaskmov[idmove] & 0xb1100) >> 2;
 
-			//float_2 f0 = curf + f_masks[idmask++] * klayf;
-			//float_2 f1 = curf + f_masks[idmask++] * klayf;
-			//float_2 f2 = curf + f_masks[idmask++] * klayf;
-			//float_2 f3 = curf + f_masks[idmask++] * klayf;
-			//idmask += 4;
+			}
+				//uint_2 dn0 = mid0 * 2;
+				//uint_2 ofs = iter2;
 
-			//src = index<2>(y += iter2.x, x += iter2.y);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-			//src = index<2>(y += iter2.x, x += iter2.y);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-			//src = index<2>(y += iter2.x, x += iter2.y);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-
-			//src = index<2>(y = 2, x = 0);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-			//src = index<2>(y += iter2.x, x += iter2.y);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-			//src = index<2>(y += iter2.x, x += iter2.y);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-			//src = index<2>(y += iter2.x, x += iter2.y);
-			//dst = index<2>(y + iter2.y, x + iter2.x);
-
-			//dst_vgpu_f[index<2>(y0, x0)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0, x0 + 1)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 1, x0)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 1, x0 + 1)] = curf + f_masks[idmask++] * klayf;
-
-			//curf = dn_vgpu_f[index<2>(y0, x0 + 1)];
-			//dst_vgpu_f[index<2>(y0, x0 + 2)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0, x0 + 3)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 1, x0 + 2)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 1, x0 + 3)] = curf + f_masks[idmask++] * klayf;
-
-			//curf = dn_vgpu_f[index<2>(y0 + 1, x0)];
-			//dst_vgpu_f[index<2>(y0 + 2, x0)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 2, x0 + 1)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 3, x0)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 3, x0 + 1)] = curf + f_masks[idmask++] * klayf;
-
-			//curf = dn_vgpu_f[index<2>(y0 + 1, x0 + 1)];
-			//dst_vgpu_f[index<2>(y0 + 2, x0 + 2)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 2, x0 + 3)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 3, x0 + 2)] = curf + f_masks[idmask++] * klayf;
-			//dst_vgpu_f[index<2>(y0 + 3, x0 + 3)] = curf + f_masks[idmask] * klayf;
+				//index<2> src = index<2>(dn0.y, dn0.x);
+				//index<2> dst = index<2>(dn0.y + ofs.y, dn0.x + ofs.x);
+				//float_2 fsrc = curf + f_masks[idmask] * klayf;
+				//float_2 fdst = curf + f_masks[idmask + 2 * ofs.y + ofs.x] * klayf;
 
 
-			// TODO: optimize!
-//			const int x0 = mad(idx[X], 2, shift.x) % dn_vgpu_a.extent[X];
-//			const int y0 = mad(idx[Y], 2, shift.y) % dn_vgpu_a.extent[Y];
-//			const int x1 = (x0 + 1) % dn_vgpu_a.extent[X];
-//			const int y1 = (y0 + 1) % dn_vgpu_a.extent[Y];
-//
-//#define idc0 (index<2>(y0, x0))
-//#define idc1 (index<2>(y0, x1))
-//#define idc2 (index<2>(y1, x0))
-//#define idc3 (index<2>(y1, x1))
-//
-//			//dn_vgpu_f[idc0] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][0] * klayf + dn_vgpu_f[idc0] * kinert;
-//			//dn_vgpu_f[idc1] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][1] * klayf + dn_vgpu_f[idc1] * kinert;
-//			//dn_vgpu_f[idc2] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][2] * klayf + dn_vgpu_f[idc2] * kinert;
-//			//dn_vgpu_f[idc3] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][3] * klayf + dn_vgpu_f[idc3] * kinert;
-//
-//			// TODO: разнести x, y, i (p.203 Greg)
-//
-//			// move vscreen points
-//#define normx0 (mad((float)(2 * x0 + 1), rSizeDn.x, -1.f))
-//#define normx1 (mad((float)(2 * x1 + 1), rSizeDn.x, -1.f))
-//#define normy0 (mad((float)(2 * y0 + 1), rSizeDn.y, -1.f))
-//#define normy1 (mad((float)(2 * y1 + 1), rSizeDn.y, -1.f))
-//
-//				//	Bottom Horizontal
-//			//auto idxsrc = idc0;
-//			//auto idxdst = idc1;
-//			//if((kf * dn_vgpu_f[idxsrc].x) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to rigth (bottom horizontal)
-//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx1;
-//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy0;
-//			//	dn_vgpu_a[idxsrc] = -1;
-//			//} else if((kf * dn_vgpu_f[idxdst].x) < 0 && dn_vgpu_a[idxsrc] < 0){ // Move to left (bottom horizontal)
-//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx0;
-//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy0;
-//			//	dn_vgpu_a[idxdst] = -1;
-//			//}
-//
-//			//// Left Vertical
-//			//idxdst = idc2;
-//			//if((kf * dn_vgpu_f[idxsrc].y) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to up (left vertical)
-//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx0;
-//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy1;
-//			//	dn_vgpu_a[idxsrc] = -1;
-//			//} else if((kf * dn_vgpu_f[idxdst].y) < 0 && dn_vgpu_a[idxsrc] < 0){	// Move to down (left vertical)
-//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx0;
-//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy0;
-//			//	dn_vgpu_a[idxdst] = -1;
-//			//}
-//
-//			//// Top Horizontal
-//			//idxsrc = idc2;
-//			//idxdst = idc3;
-//			//if((kf * dn_vgpu_f[idxsrc].x) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to right (top horizontal)
-//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx1;
-//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy1;
-//			//	dn_vgpu_a[idxsrc] = -1;
-//			//} else if((kf * dn_vgpu_f[idxdst].x) < 0 && dn_vgpu_a[idxsrc] < 0){	// Move to left (top horizontal)
-//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx0;
-//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy1;
-//			//	dn_vgpu_a[idxdst] = -1;
-//			//}
-//
-//			//// Right Vertical
-//			//idxsrc = idc1;
-//			//if((kf * dn_vgpu_f[idxsrc].y) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to up (right vertical)
-//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx1;
-//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy1;
-//			//	dn_vgpu_a[idxsrc] = -1;
-//			//} else if((kf * dn_vgpu_f[idxdst].y) < 0 && dn_vgpu_a[idxsrc] < 0){	// Move to down (right vertical)
-//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx1;
-//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy0;
-//			//	dn_vgpu_a[idxdst] = -1;
-//			//}
+				//float_2 f0 = curf + f_masks[idmask++] * klayf;
+				//float_2 f1 = curf + f_masks[idmask++] * klayf;
+				//float_2 f2 = curf + f_masks[idmask++] * klayf;
+				//float_2 f3 = curf + f_masks[idmask++] * klayf;
+				//idmask += 4;
+
+				//src = index<2>(y += iter2.x, x += iter2.y);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+				//src = index<2>(y += iter2.x, x += iter2.y);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+				//src = index<2>(y += iter2.x, x += iter2.y);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+
+				//src = index<2>(y = 2, x = 0);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+				//src = index<2>(y += iter2.x, x += iter2.y);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+				//src = index<2>(y += iter2.x, x += iter2.y);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+				//src = index<2>(y += iter2.x, x += iter2.y);
+				//dst = index<2>(y + iter2.y, x + iter2.x);
+
+				//dst_vgpu_f[index<2>(y0, x0)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0, x0 + 1)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 1, x0)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 1, x0 + 1)] = curf + f_masks[idmask++] * klayf;
+
+				//curf = dn_vgpu_f[index<2>(y0, x0 + 1)];
+				//dst_vgpu_f[index<2>(y0, x0 + 2)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0, x0 + 3)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 1, x0 + 2)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 1, x0 + 3)] = curf + f_masks[idmask++] * klayf;
+
+				//curf = dn_vgpu_f[index<2>(y0 + 1, x0)];
+				//dst_vgpu_f[index<2>(y0 + 2, x0)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 2, x0 + 1)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 3, x0)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 3, x0 + 1)] = curf + f_masks[idmask++] * klayf;
+
+				//curf = dn_vgpu_f[index<2>(y0 + 1, x0 + 1)];
+				//dst_vgpu_f[index<2>(y0 + 2, x0 + 2)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 2, x0 + 3)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 3, x0 + 2)] = curf + f_masks[idmask++] * klayf;
+				//dst_vgpu_f[index<2>(y0 + 3, x0 + 3)] = curf + f_masks[idmask] * klayf;
+
+
+				// TODO: optimize!
+	//			const int x0 = mad(idx[X], 2, shift.x) % dn_vgpu_a.extent[X];
+	//			const int y0 = mad(idx[Y], 2, shift.y) % dn_vgpu_a.extent[Y];
+	//			const int x1 = (x0 + 1) % dn_vgpu_a.extent[X];
+	//			const int y1 = (y0 + 1) % dn_vgpu_a.extent[Y];
+	//
+	//#define idc0 (index<2>(y0, x0))
+	//#define idc1 (index<2>(y0, x1))
+	//#define idc2 (index<2>(y1, x0))
+	//#define idc3 (index<2>(y1, x1))
+	//
+	//			//dn_vgpu_f[idc0] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][0] * klayf + dn_vgpu_f[idc0] * kinert;
+	//			//dn_vgpu_f[idc1] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][1] * klayf + dn_vgpu_f[idc1] * kinert;
+	//			//dn_vgpu_f[idc2] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][2] * klayf + dn_vgpu_f[idc2] * kinert;
+	//			//dn_vgpu_f[idc3] = up_vgpu_f[idx] + f_masks[up_vgpu_a[idx]][3] * klayf + dn_vgpu_f[idc3] * kinert;
+	//
+	//			// TODO: разнести x, y, i (p.203 Greg)
+	//
+	//			// move vscreen points
+	//#define normx0 (mad((float)(2 * x0 + 1), rSizeDn.x, -1.f))
+	//#define normx1 (mad((float)(2 * x1 + 1), rSizeDn.x, -1.f))
+	//#define normy0 (mad((float)(2 * y0 + 1), rSizeDn.y, -1.f))
+	//#define normy1 (mad((float)(2 * y1 + 1), rSizeDn.y, -1.f))
+	//
+	//				//	Bottom Horizontal
+	//			//auto idxsrc = idc0;
+	//			//auto idxdst = idc1;
+	//			//if((kf * dn_vgpu_f[idxsrc].x) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to rigth (bottom horizontal)
+	//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx1;
+	//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy0;
+	//			//	dn_vgpu_a[idxsrc] = -1;
+	//			//} else if((kf * dn_vgpu_f[idxdst].x) < 0 && dn_vgpu_a[idxsrc] < 0){ // Move to left (bottom horizontal)
+	//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx0;
+	//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy0;
+	//			//	dn_vgpu_a[idxdst] = -1;
+	//			//}
+	//
+	//			//// Left Vertical
+	//			//idxdst = idc2;
+	//			//if((kf * dn_vgpu_f[idxsrc].y) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to up (left vertical)
+	//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx0;
+	//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy1;
+	//			//	dn_vgpu_a[idxsrc] = -1;
+	//			//} else if((kf * dn_vgpu_f[idxdst].y) < 0 && dn_vgpu_a[idxsrc] < 0){	// Move to down (left vertical)
+	//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx0;
+	//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy0;
+	//			//	dn_vgpu_a[idxdst] = -1;
+	//			//}
+	//
+	//			//// Top Horizontal
+	//			//idxsrc = idc2;
+	//			//idxdst = idc3;
+	//			//if((kf * dn_vgpu_f[idxsrc].x) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to right (top horizontal)
+	//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx1;
+	//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy1;
+	//			//	dn_vgpu_a[idxsrc] = -1;
+	//			//} else if((kf * dn_vgpu_f[idxdst].x) < 0 && dn_vgpu_a[idxsrc] < 0){	// Move to left (top horizontal)
+	//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx0;
+	//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy1;
+	//			//	dn_vgpu_a[idxdst] = -1;
+	//			//}
+	//
+	//			//// Right Vertical
+	//			//idxsrc = idc1;
+	//			//if((kf * dn_vgpu_f[idxsrc].y) > 0 && dn_vgpu_a[idxdst] < 0){	// Move to up (right vertical)
+	//			//	screen[dn_vgpu_a[idxdst] = dn_vgpu_a[idxsrc]].Pos.x = normx1;
+	//			//	screen[dn_vgpu_a[idxdst]].Pos.y = normy1;
+	//			//	dn_vgpu_a[idxsrc] = -1;
+	//			//} else if((kf * dn_vgpu_f[idxdst].y) < 0 && dn_vgpu_a[idxsrc] < 0){	// Move to down (right vertical)
+	//			//	screen[dn_vgpu_a[idxsrc] = dn_vgpu_a[idxdst]].Pos.x = normx1;
+	//			//	screen[dn_vgpu_a[idxsrc]].Pos.y = normy0;
+	//			//	dn_vgpu_a[idxdst] = -1;
+	//			//}
 		});
 } // ////////////////////////////////////////////////////////////////////////////
 #undef normx0
