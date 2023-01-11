0d7d814f8368db4165861ce92a7e35c62813c31d
VVVofka <vvvofka@yandex.ru>
2023-01-11 11:10:45 +0300

    sync

--- AMPEngine2Lib/Engine/ProcessF.h
+++ AMPEngine2Lib/Engine/ProcessF.h
@@ -17,7 +17,6 @@ public:
 
 private:
 	void gpuRun0(const int_2 shift, const uint iter);
-	//void gpuRun0Bak(const int_2 shift, const uint iter);
 	void gpuRun0Split1(const int_2 shift);
 	void gpuRun0Split2(const int_2 shift, const uint iter);
 	void gpuRun1(const int ncurlay);
--- AMPEngine2Lib/Engine/ProcessF0.cpp
+++ AMPEngine2Lib/Engine/ProcessF0.cpp
@@ -1,7 +1,4 @@
-#include <amp_math.h>
 #include "ProcessF.h"
-#include "..\myUtil.h"	// NORMAL_TO_AREA
-#include "EngineDbg.h"	// VVVDBG_DUMP
 void ProcessF::gpuRun0(const int_2 shift0, const uint iter){
 	gpuRun0Split1(shift0);
 	gpuRun0Split2(shift0, iter);
